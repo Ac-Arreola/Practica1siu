642310e809ab307fbbd984a96ce32e3c385fbd21
Ac-Arreola <120034358@upq.edu.mx>
2022-10-28 17:10:14 -0500

    se crearon 2 vistas

---
+++ resources/views/Agregar.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>AGREGAR</title>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    <body class="bg-info ">
+        <div class="container mt-5 mb-5">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title mt-2">Ingresa tus credenciales</h5>
+            </div>
+        
+            <div class="card-body">
+              
+              
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label fw-bold"> Correo electrónico </label>
+                        <input type="email" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label fw-bold"> Contraseña </label>
+                        <input type="password" class="form-control">
+                    </div>
+                    <div class="d-grid gap-2 col-6 mx-auto">
+                    <button type="submit" class="btn btn-info fw-bold">Submit</button>
+                    </div>
+                </form>
+            </div>
+          </div>
+        </div>
+       
+
+        <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ resources/views/Tabla.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TABLA</title>
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    <body class="bg-info ">
+        <div class="container mt-5 mb-5 bg-light ">
+            <table class="table table-striped table-hover mb-4">
+                <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Correo</th>
+                      <th scope="col">Contraseña</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Bicho@gmail.com</td>
+                      <td>siuuuu</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Ladybug@gmail.com</td>
+                      <td>Tikkimotassiiiii</td>
+                    </tr>
+                   
+                  </tbody>
+              </table>
+        </div>
+       
+
+        <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
